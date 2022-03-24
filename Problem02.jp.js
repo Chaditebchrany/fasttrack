0568b96ec7119193c2b44f0a0996c74dcf976981
Chaditebchrany <102185725+Chaditebchrany@users.noreply.github.com>
2022-03-24 10:23:12 +0200

    Add files via upload

---
+++ Problem02.jp.js
@@ -0,0 +1,13 @@
+let num1 = 10
+let num2 = 7
+let num3 = 1
+if ( num1 > num2 + num3);
+console.log(true);
+else if ( num2 = (num3)^2);
+console.log(false);
+else if (num1 + num2 + num3 < num1^8 );
+console.log(undefined);
+else 
+console.log(null)
+
+
